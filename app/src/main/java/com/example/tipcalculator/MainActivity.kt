0f1497f4dd0d9234d9309a02bc13cfb8ee7ea115
ratminen <ghgpoklonik16@gmail.com>
2025-09-29 14:33:07 +0700

    2. Column title

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TipCalculatorTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MainScreen(modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -31,17 +32,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text ="Сумма заказа:",
+            modifier = Modifier
+        )
+        Text(
+            text ="Количество блюд:",
+            modifier = Modifier
+        )
+        Text(
+            text ="Чаевые:",
+            modifier = Modifier
+        )
+        Text(
+            text ="Скидка:",
+            modifier = Modifier,
+            fontSize = 20.sp,
+        )
+
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     TipCalculatorTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
